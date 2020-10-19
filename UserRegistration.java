412d2e836991c183f798bd4be8d3d9663d232458
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-19 12:31:57 +0530

    Added to check password validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,8 @@ public class UserRegistration {
 	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
 	private static final String MOB_NUM_PATTERN = "^[0-9]{2}[ ]*[6-9][0-9]{9}$";
+    private static final String PASSWORD_PATTERN1 = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
+
 	public boolean validateFirstName(String fName) {
 		Pattern pattern = Pattern.compile(NAME_PATTERN);
 		return pattern.matcher(fName).matches();
@@ -24,6 +26,14 @@ public class UserRegistration {
 	        Pattern pattern = Pattern.compile(MOB_NUM_PATTERN);
 	        return pattern.matcher(mobNum).matches();
 	    }
+	 
+	 public boolean validatePassword(String password) {
+	        Pattern pattern1 = Pattern.compile(PASSWORD_PATTERN1);
+	        return pattern1.matcher(password).matches();
+	  
+	    }
+	 
+
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -56,6 +66,13 @@ public class UserRegistration {
 			System.out.println("Entered mobile number is valid");
 		else
 			System.out.println("Entered mobile number is invalid");
+		System.out.println("Enter password:");
+		String str5=sc.next();
+		boolean result4=userregi.validatePassword(str5);
+		if(result4==true)
+			System.out.println("Entered password is valid");
+		else
+			System.out.println("Entered password is invalid");
 
 	}
 
