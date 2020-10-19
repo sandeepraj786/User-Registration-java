3d2d8d4aa9cf16602511090e879f2de3c715a70c
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-19 11:11:14 +0530

    Added to check first name validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration problem");
+	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+
+	public boolean validateFirstName(String fName) {
+		Pattern pattern = Pattern.compile(NAME_PATTERN);
+		return pattern.matcher(fName).matches();
 	}
-	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first name");
+		String str1=sc.next();
+		UserRegistration userregi=new UserRegistration();
+		boolean result=userregi.validateFirstName(str1);
+		if(result==true)
+			System.out.println("Entered first name is valid");
+		else
+			System.out.println("Entered first name is invalid");
+
+	}
 
 }
